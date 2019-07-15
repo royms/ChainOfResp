3a2daffb1cf75f4d8a6613f65132eb335cfff734
royms <hadash04@gmail.com>
2019-07-15 10:03:33 +0300

    chainOfResp

---
+++ ChainOfResp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BillHandlerBase billHandler500 = new BillHandle500();
+            BillHandlerBase billHandler200 = new BillHandler200();
+            BillHandlerBase billHandler100 = new BillHandle100();
+            BillHandlerBase billHandler25 = new BillHandle25();
+            BillHandlerBase coinHandler5 = new CoinHandler5();
+            BillHandlerBase billHandler2 = new BillHandle2();
+
+            BillHandlerBase chainRoot = billHandler500;
+            billHandler500.SetNext(billHandler200);
+            billHandler200.SetNext(billHandler100);
+            billHandler100.SetNext(billHandler25);
+            billHandler25.SetNext(coinHandler5);
+
+            Console.WriteLine("385:");
+            chainRoot.Handle(385);
+            Console.WriteLine("==================");
+            Console.WriteLine("400:");
+            chainRoot.Handle(400);
+            Console.WriteLine("==================");
+            Console.WriteLine("402:");
+            chainRoot.Handle(402);
+            Console.WriteLine("==================");
+            Console.WriteLine("500:");
+            chainRoot.Handle(500);
+        }
+    }
+}
